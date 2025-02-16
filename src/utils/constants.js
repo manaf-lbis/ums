ea6fc1af03225b8c7919eddbb6253c6375d5488c
manaf-lbis <manaf.lbis@mail.com>
2025-02-16 08:37:07 +0530

    Updated API URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,7 @@
 // export const IMG_URL = "http://localhost:3000/images/"
 // export const API_URL  = 'https://server-5fk2k38vs-codebrocamp-gmailcoms-projects.vercel.app'
 // export const IMG_URL = "https://server-5fk2k38vs-codebrocamp-gmailcoms-projects.vercel.app/images/"
-export const API_URL  = '"https://server-60j6msyzb-codebrocamp-gmailcoms-projects.vercel.app"'
-export const IMG_URL = "https://server-60j6msyzb-codebrocamp-gmailcoms-projects.vercel.app/images/"
+// export const API_URL  = '"https://server-60j6msyzb-codebrocamp-gmailcoms-projects.vercel.app"'
+// export const IMG_URL = "https://server-60j6msyzb-codebrocamp-gmailcoms-projects.vercel.app/images/"
+export const API_URL  = "https://server-8cpr76giu-codebrocamp-gmailcoms-projects.vercel.app"
+export const IMG_URL = "https://server-8cpr76giu-codebrocamp-gmailcoms-projects.vercel.app/images/"
