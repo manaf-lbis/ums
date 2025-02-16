0a2becc03db1d8e98cff890977e557ebd8e59349
manaf-lbis <manaf.lbis@mail.com>
2025-02-16 09:26:16 +0530

    Updated API Ulastt

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -13,6 +13,8 @@
 // export const IMG_URL = "https://server-pid2ed5wh-codebrocamp-gmailcoms-projects.vercel.app/images/"
 // export const API_URL  = "https://server-mz2plsib8-codebrocamp-gmailcoms-projects.vercel.app"
 // export const IMG_URL = "https://server-mz2plsib8-codebrocamp-gmailcoms-projects.vercel.app/images/"
-export const API_URL  = "https://server-1sj5qrgkj-codebrocamp-gmailcoms-projects.vercel.app"
-export const IMG_URL = "https://server-1sj5qrgkj-codebrocamp-gmailcoms-projects.vercel.app/images/"
+// export const API_URL  = "https://server-1sj5qrgkj-codebrocamp-gmailcoms-projects.vercel.app"
+// export const IMG_URL = "https://server-1sj5qrgkj-codebrocamp-gmailcoms-projects.vercel.app/images/"
+export const API_URL  = "https://server-2wwr1nl6i-codebrocamp-gmailcoms-projects.vercel.app"
+export const IMG_URL = "https://server-2wwr1nl6i-codebrocamp-gmailcoms-projects.vercel.app/images/"
 
